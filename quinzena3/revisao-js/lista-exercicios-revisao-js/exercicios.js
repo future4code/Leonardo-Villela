69df50fb049651f8f6ae78263426b2ac21a4827b
leonardo mosso <leonardo_mcv@hotmail.com>
2021-06-18 11:55:58 -0300

    Adicionando exercício 14

--- quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
+++ quinzena3/revisao-js/lista-exercicios-revisao-js/exercicios.js
@@ -246,6 +246,13 @@ function imprimeChamada() {
 // EXERCÍCIO 14
 function criaRetangulo(lado1, lado2) {
 
+  const informacoesRetangulo = {
+    largura: lado1,
+    altura: lado2,
+    perimetro: 2 * (lado1 + lado2),
+    area: lado1 * lado2
+  }
+  return informacoesRetangulo
 }
 
 // EXERCÍCIO 15
